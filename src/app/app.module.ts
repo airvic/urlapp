2023c573ece9f2f939ef3787cde334eb6b37fa45
airvic <airewamhevictor@gmail.com>
2022-11-21 06:19:53 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import {NgTinyUrlModule} from 'ng-tiny-url';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { AboutComponent } from './about/about.component'
+import { AboutComponent } from './about/about.component';
+import { PractiseComponent } from './practise/practise.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    PractiseComponent
   ],
   imports: [
     BrowserModule,
